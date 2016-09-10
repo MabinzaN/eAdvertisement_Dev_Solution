10ea21c9abb7a2d2f67b13afcb7b5165b24cad8d
Nathi <mabinza.nathi6@gmail.com>
2016-09-10 13:53:19 +0200

    Add project files.

---
+++ eAdertisement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eAdertisement.MvcApplication" Language="C#" %>
---
+++ eAdertisement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eAdertisement.Startup))]
+namespace eAdertisement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
